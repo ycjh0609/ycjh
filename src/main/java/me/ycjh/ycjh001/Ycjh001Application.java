5d379b55f3f67c75932f3aac3675c2f2f50c3807
cayc1213 <cayc1213@gmail.com>
2019-06-09 15:24:05 +0900

    setting environment

---
+++ src/main/java/me/ycjh/ycjh001/Ycjh001Application.java
@@ -0,0 +1,13 @@
+package me.ycjh.ycjh001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ycjh001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ycjh001Application.class, args);
+    }
+
+}
